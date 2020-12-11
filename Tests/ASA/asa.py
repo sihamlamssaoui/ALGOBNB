37bc2295f4f1f35932874c35beab67aa128fb4a1
Cristian Lepore <clepore@localhost.localdomain>
2020-12-11 22:09:48 +0000

    .

--- Tests/ASA/asa.py
+++ Tests/ASA/asa.py
@@ -1,24 +1,21 @@
 from pyteal import *
 
+
 def initial_approval_program():
     on_creation = Return(Int(1))
-    
     on_deletion = Return(Int(1))
-    
     on_update = Return(Int(1))
-    
     on_closeout = Return(Int(1))
 
     on_optin = Seq([App.localPut(Int(0), Bytes("address"), Txn.sender()),
-		Return(Int(1))])
+                    Return(Int(1))])
 
     on_clear = Return(Int(1))
 
     get_address_of_sender = App.localGetEx(Int(0), App.id(), Bytes("address"))
 
-
-    asa_allowed_address = Return(And(get_address_of_sender.hasValue() , get_address_of_sender.value() == Gtxn[1].sender()))
-
+    asa_allowed_address = Return(And(get_address_of_sender.hasValue(
+    ), get_address_of_sender.value() == Gtxn[1].sender()))
 
     program = Cond(
         [Txn.application_id() == Int(0), on_creation],
@@ -27,10 +24,12 @@ def initial_approval_program():
         [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
         [Txn.on_completion() == OnComplete.OptIn, on_optin],
         [Txn.on_completion() == OnComplete.ClearState, on_clear],
-        [Txn.application_args[0] == Bytes("asa_allowed_address"), asa_allowed_address]
+        [Txn.application_args[0] == Bytes(
+            "asa_allowed_address"), asa_allowed_address]
     )
 
     return program
+
 
 def clear_state_program():
     return Int(1)
@@ -41,30 +40,5 @@ with open('asa.teal', 'w') as f:
     f.write(compiled)
 
 with open('asa_clear.teal', 'w') as f:
-        compiled = compileTeal(clear_state_program(), Mode.Application)
-        f.write(compiled)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    compiled = compileTeal(clear_state_program(), Mode.Application)
+    f.write(compiled)
